e39db6a53d570dfa6ee5ed1be77c76bb8419ee2c
amyjprogrammer <amyjprogrammer@gmail.com>
2021-06-30 00:09:53 -0400

    Added Game Board and Mario

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1],
 })
+
+const MOVE_SPEED = 120
+const JUMP_FORCE = 360
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
@@ -30,22 +33,97 @@ scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
     const map = [
-        '                            ',
-        '                            ',
-        '     %  =*=%=               ',
-        '                            ',
-        '                            ',
-        '                   -+       ',
-        '              ^  ^ ()       ',
-        '===================== ======',
+        '                                  ',
+        '                                  ',
+        '                                  ',
+        '                                  ',
+        '                                  ',
+        '     %  =*=%=                     ',
+        '                                  ',
+        '                                  ',
+        '                         -+       ',
+        '                    ^  ^ ()       ',
+        '===========================  =====',
     ]
 
     const levelCfg = {
         width: 20,
         height: 20,
         '=': [sprite('block'), solid()],
-        '$': [sprite()]
+        '$': [sprite('coin')],
+        '%': [sprite('surprise'), solid(), 'coin-surprise'],
+        '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+        '}': [sprite('unboxed'), solid()],
+        '^': [sprite('evil-shroom'), solid()],
+        '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+        ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
+        '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
+        '#': [sprite('mushroom'), solid()],
     }
+
+    const gameLevel = addLevel(map, levelCfg)
+
+    const scoreLabel = add([
+        text('test'),
+        pos(30, 6),
+        layer('ui'),
+        {
+            value: 'test',
+        }
+    ])
+
+    add ([text('level ' +  'test', pos(4,6))])
+
+    function big() {
+        let timer = 0
+        let isBig = false
+        return {
+            update() {
+                if (isBig) {
+                    timer -= dt()
+                    if (timer <= 0){
+                        this.smallify()
+                    }
+                }
+            },
+            isBig() {
+                return isBig
+            },
+            smallify() {
+                this.scale = vec2(1)
+                timer = 0
+                isBig = false
+            },
+            bigify(time) {
+                this.scale = vec2(2)
+                timer = time
+                isBig = true
+            }
+        }
+    }
+
+    const player = add([
+        sprite('mario'), solid(), 
+        pos(30, 0),
+        body(),
+        big(),
+        origin('bot')
+    ])
+
+    keyDown('left', () => {
+        player.move(-MOVE_SPEED, 0)
+    })
+
+    keyDown('right', () => {
+        player.move(MOVE_SPEED, 0)
+    })
+
+    keyPress('space', () => {
+        if(player.grounded()) {
+            player.jump(JUMP_FORCE) 
+        }
+    })
 
 })
 
